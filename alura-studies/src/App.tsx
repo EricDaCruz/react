7fc03d93dbb4d7a5472ef620cca7374299b027dd
Eric Rafael <rafaeleric40@gmail.com>
2023-02-06 08:32:25 -0300

    :tada: Commit inicial

---
+++ alura-studies/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button";
+
+function App() {
+
+   return (
+      <div className="">
+         <Button />
+      </div>
+   );
+}
+
+export default App;
---
+++ alura-studies/src/components/Button/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Button extends React.Component {
+   render(): React.ReactNode {
+      return <button>Click here</button>;
+   }
+}
