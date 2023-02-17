177b3fba99c33eb75dfe2b3016ba55817dbef28d
Eric Rafael <rafaeleric40@gmail.com>
2023-02-17 09:34:34 -0300

    :sparkles: feat: Map for rendering list items

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,17 +1,36 @@
+interface Task {
+   title: string;
+   time: string;
+}
+
 export function List() {
+   const tasks: Task[] = [
+      {
+         title: "React",
+         time: "02:00:00",
+      },
+      {
+         title: "Javascript",
+         time: "01:00:00",
+      },
+      {
+         title: "Typescript",
+         time: "03:00:00",
+      },
+   ];
+
    return (
       <aside>
          <h2>Study of days</h2>
-
          <ul>
-            <li>
-               <h3>React</h3>
-               <span>02:00:00</span>
-            </li>
-            <li>
-               <h3>Typescript</h3>
-               <span>01:20:00</span>
-            </li>
+            {tasks.map((task, key) => {
+               return (
+                  <li key={key}>
+                     <h3>{task.title}</h3>
+                     <span>{task.time}</span>
+                  </li>
+               );
+            })}
          </ul>
       </aside>
    );
