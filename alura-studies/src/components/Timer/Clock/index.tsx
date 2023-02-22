d94fc03c302f675aab51ec69b715896bbda2b51b
Eric Rafael <rafaeleric40@gmail.com>
2023-02-22 08:54:27 -0300

    :sparkles: feat: Show on clock

--- alura-studies/src/components/Timer/Clock/index.tsx
+++ alura-studies/src/components/Timer/Clock/index.tsx
@@ -1,13 +1,20 @@
+import { Task } from "../../../types/task";
 import style from "./Clock.module.scss";
 
-export function Clock() {
+export function Clock({ time = 0 }: { time: number | undefined }) {
+   const minutes = Math.floor(time / 60);
+   const seconds = time % 60;
+
+   const [minutesTen, minutesUnit] = String(minutes).padStart(2, "0");
+   const [secondsTen, secondsUnit] = String(seconds).padStart(2, "0");
+
    return (
       <>
-         <span className={style.relogioNumero}>0</span>
-         <span className={style.relogioNumero}>0</span>
+         <span className={style.relogioNumero}>{minutesTen}</span>
+         <span className={style.relogioNumero}>{minutesUnit}</span>
          <span className={style.relogioDivisao}>:</span>
-         <span className={style.relogioNumero}>0</span>
-         <span className={style.relogioNumero}>0</span>
+         <span className={style.relogioNumero}>{secondsTen}</span>
+         <span className={style.relogioNumero}>{secondsUnit}</span>
       </>
    );
 }
--- alura-studies/src/components/Timer/index.tsx
+++ alura-studies/src/components/Timer/index.tsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
+import { Task } from "../../types/task";
 import { timeToSeconds } from "../../utils/time";
 import { Button } from "../Button";
 import { Clock } from "./Clock";
 
 import style from "./Timer.module.scss";
 
-export function Timer() {
-   
+export function Timer({ selected }: { selected: Task | undefined }) {
+   const [time, setTime] = useState<number>();
+
+   useEffect(() => {
+      if (selected?.time) {
+         setTime(timeToSeconds(selected.time));
+      }
+   }, [selected]);
+
    return (
       <div className={style.cronometro}>
          <p className={style.titulo}>Choose a card and start the timer:</p>
          <div className={style.relogioWrapper}>
-            <Clock />
+            <Clock time={time}/>
          </div>
-         <Button text="Start" type="button"/>
+         <Button text="Start" type="button" />
       </div>
    );
 }
